b194f58257438c7174f8f7045348b2679051ab6d
Mike Joyce <contact@mikejoyce.me>
2022-01-25 10:41:30 +0700

    Remove console.log

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,8 +3,6 @@ import React from 'react'
 export default function Image (props) {
   const { image, className } = props
 
-  console.log(image)
-
   return (
     <>
       <img className={className} src={`/images/${image}`}  />
